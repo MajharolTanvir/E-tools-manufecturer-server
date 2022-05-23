55afb1b9e9e47d2f407903da92407ba9e9b3dc21
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-24 00:04:25 +0600

    save get orders data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 var cors = require('cors')
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middle wear
 app.use(cors())
@@ -17,10 +17,29 @@ async function run() {
         await client.connect();
 
         const toolsCollection = client.db('Manufacturer').collection('tools')
+        const ordersCollection = client.db('Manufacturer').collection('orders')
 
+        // ----------------Get all tools data-------------------------------
         app.get('/tools', async (req, res) => {
             const query = await toolsCollection.find().toArray()
             res.send(query)
+        })
+
+        // ---------------Get a single tool data-----------------------------
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await toolsCollection.findOne(query)
+            res.send(result)
+        })
+        // ------------------------Get all order collection -----------------------------
+
+        
+        // ------------------------Post order collection -----------------------------
+        app.post('/orders', async (req, res) => {
+            const data = req.body
+            const result = await ordersCollection.insertOne(data)
+            res.send(result)
         })
     }
 
