3c43d0232b83463e7233377e8d81d4f321c19173
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-24 20:42:10 +0600

    get order data done

--- index.js
+++ index.js
@@ -19,27 +19,62 @@ async function run() {
 
         const toolsCollection = client.db('Manufacturer').collection('tools')
         const ordersCollection = client.db('Manufacturer').collection('orders')
+        const userCollection = client.db('Manufacturer').collection('user')
 
-        // ----------------Get all tools data-------------------------------
+        // -----------------------Get all tools data-------------------------------
         app.get('/tools', async (req, res) => {
             const query = await toolsCollection.find().toArray()
             res.send(query)
         })
 
-        // ---------------Get a single tool data-----------------------------
+        // ---------------------Get a single tool data-----------------------------
         app.get('/tool/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await toolsCollection.findOne(query)
             res.send(result)
         })
-        // ------------------------Get all order collection -----------------------------
-
         
-        // ------------------------Post order collection -----------------------------
+        // ------------------------Get all order collection ------------------------
+        app.get('/order/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await ordersCollection.find(filter).toArray()
+            res.send(result)
+        })
+
+        // ------------------------Post order collection ---------------------------
         app.post('/orders', async (req, res) => {
             const data = req.body
             const result = await ordersCollection.insertOne(data)
+            res.send(result)
+        })
+
+        // ------------------------Get all user data---------------------------------
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+            const result = await userCollection.findOne(filter)
+            res.send(result)
+        })
+
+        // ------------------------Post user data------------------------------------
+        app.post('/user', async (req, res) => {
+            const data = req.body
+            const result = await userCollection.insertOne(data)
+            res.send(result)
+        })
+
+        // ------------------------Put user data-------------------------------------
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
             res.send(result)
         })
     }
